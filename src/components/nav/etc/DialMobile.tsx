ba98e437fd91bd207ac63db410b0d77c5927cd0b
wallodya <78960605+wallodya@users.noreply.github.com>
2023-03-20 16:44:09 +0300

    fix(DialMobile):removed mui

--- src/components/nav/etc/DialMobile.tsx
+++ src/components/nav/etc/DialMobile.tsx
@@ -1,18 +1,13 @@
-import ListAltRounded from "@mui/icons-material/ListAltRounded";
-import TaskRounded from "@mui/icons-material/TaskRounded";
-import SpeedDial from "@mui/material/SpeedDial";
-import SpeedDialAction from "@mui/material/SpeedDialAction";
-import SpeedDialIcon from "@mui/material/SpeedDialIcon";
 import React, { useState } from "react";
 
 const actions = [
     {
         name: "Task",
-        icon: <TaskRounded/>
+        icon: <></>
     },
     {
         name: "Quest",
-        icon: <ListAltRounded/>
+        icon: <></>
     }
 ]
 
@@ -22,30 +17,31 @@ const DialMobile = () => {
     const handleClose = () => setIsOpen(false)
 
     return (
-        <SpeedDial
-            ariaLabel="Speed dial add new"
-            icon={<SpeedDialIcon />}
-            sx={{ position: "absolute", bottom: 16, right: 16 }}
-            FabProps={{
-                className: "bg-sky-400",
-            }}
-            onClose={handleClose}
-            onOpen={handleOpen}
-            open={isOpen}
-        >
-            {actions.map((action) => (
-                <SpeedDialAction
-                    key={action.name}
-                    tooltipTitle={action.name}
-                    tooltipOpen
-                    icon={action.icon}
-                    FabProps={{
-                        className: "dark:bg-slate-100 dark:text-slate-800 bg-slate-800 bg-slate-100",
-                        classes: {}
-                    }}
-                />
-            ))}
-        </SpeedDial>
+        // <SpeedDial
+        //     ariaLabel="Speed dial add new"
+        //     icon={<SpeedDialIcon />}
+        //     sx={{ position: "absolute", bottom: 16, right: 16 }}
+        //     FabProps={{
+        //         className: "bg-sky-400",
+        //     }}
+        //     onClose={handleClose}
+        //     onOpen={handleOpen}
+        //     open={isOpen}
+        // >
+        //     {actions.map((action) => (
+        //         <SpeedDialAction
+        //             key={action.name}
+        //             tooltipTitle={action.name}
+        //             tooltipOpen
+        //             icon={action.icon}
+        //             FabProps={{
+        //                 className: "dark:bg-slate-100 dark:text-slate-800 bg-slate-800 bg-slate-100",
+        //                 classes: {}
+        //             }}
+        //         />
+        //     ))}
+        // </SpeedDial>
+        <></>
     );
 };
 
