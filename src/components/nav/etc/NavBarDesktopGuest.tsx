755f4a62a7b8f6e746c6539daa2b9a5af50e1542
wallodya <78960605+wallodya@users.noreply.github.com>
2023-03-20 16:50:18 +0300

    fix:removed mui

--- src/components/nav/etc/NavBarDesktopGuest.tsx
+++ src/components/nav/etc/NavBarDesktopGuest.tsx
@@ -1,22 +1,15 @@
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import Link from "next/link";
 import React from "react";
 import { useSignInMutation } from "../../../store/api/api.slice";
 
+// Should have a Button element
 const NavBarDesktopGuest = () => {
     return (
         <div className="col-start-3 flex justify-end items-center">
             <Link href="/sign-in">
-                <Button variant="outlined" color="inherit">
-                    <Typography
-                        variant="subtitle2"
-                        component="span"
-                        className="ml-auto font-bold text-slate-800 dark:text-slate-100"
-                    >
-                        Sign in
-                    </Typography>
-                </Button>
+                        <span className="ml-auto font-bold text-slate-800 dark:text-slate-100">
+                            Sign in
+                        </span>
             </Link>
         </div>
     );
