c75f9227ade1c72069aa7ddd36ac8be1ca7ffb4a
wallodya <78960605+wallodya@users.noreply.github.com>
2023-03-20 16:52:23 +0300

    fix(HeaderDesktop):removed mui

--- src/components/nav/header/HeaderDesktop.tsx
+++ src/components/nav/header/HeaderDesktop.tsx
@@ -1,15 +1,16 @@
 "use-client";
 
-import AppBar from "@mui/material/AppBar";
 import Link from "next/link";
 import React from "react";
 import MenuDesktop from "../menu/MenuDesktop";
 
 const Header = () => {
     return (
-        <AppBar position="fixed" color="inherit" className="hidden sm:grid grid-cols-main bg-none bg-slate-100 dark:bg-slate-800 text-slate-800 dark:text-slate-100 shadow-sm shadow-slate-300 dark:shadow-slate-900">
+        <div className="hidden sm:grid grid-cols-main bg-none bg-slate-100 dark:bg-slate-800 text-slate-800 dark:text-slate-100 shadow-sm shadow-slate-300 dark:shadow-slate-900">        
             <MenuDesktop />
-        </AppBar>
+        </div>
+
+
     );
 };
 
