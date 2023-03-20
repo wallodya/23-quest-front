1112ef52d0174b22d8d8a5a8d89f648bd9f458f1
wallodya <78960605+wallodya@users.noreply.github.com>
2023-03-20 16:54:56 +0300

    fix(HeaderMobile):removed mui

--- src/components/nav/header/HeaderMobile.tsx
+++ src/components/nav/header/HeaderMobile.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-import MenuRounded from "@mui/icons-material/MenuRounded";
-import Button from "@mui/material/Button";
-import AppBar from "@mui/material/AppBar";
-import Typography from "@mui/material/Typography";
 import Link from "next/link";
 import { useAppSelector } from "../../../store/hooks";
 import DrawerSideMobie, { useMobileDrawer } from "../menu/DrawerSideMobie";
@@ -15,39 +11,34 @@ const HeaderMobile = () => {
     const { toggleDrawer, isDrawerOpen } = useMobileDrawer();
 
     return (
-        <AppBar className="grid grid-cols-main bg-slate-100 bg-none py-4 text-slate-800 shadow-sm shadow-slate-300 dark:bg-slate-800 dark:text-slate-100 dark:shadow-slate-900 sm:hidden">
+        // Add an AppBar
+        <div className="grid grid-cols-main bg-slate-100 bg-none py-4 text-slate-800 shadow-sm shadow-slate-300 dark:bg-slate-800 dark:text-slate-100 dark:shadow-slate-900 sm:hidden">
             {isSignedIn ? (
                 <div className="col-start-2 flex items-center">
-                    <Button
-                        variant="text"
-                        color="inherit"
+                    <button
                         onClick={toggleDrawer}
                     >
-                        <MenuRounded />
-                    </Button>
-                    <Typography
-                        variant="subtitle2"
-                        component="span"
+                        <></> /**Add icon */
+                    </button>
+                    <span
                         className="font-bold text-slate-800 dark:text-slate-100"
                     >
                         {login}
-                    </Typography>
+                    </span>
                     <DrawerSideMobie toggleDrawer={toggleDrawer} isDrawerOpen={isDrawerOpen} />
                 </div>
             ) : (
                 <div className="col-start-2 flex">
                     <Link href="sign-in" className="ml-auto flex">
-                        <Typography
-                            variant="subtitle2"
-                            component="span"
+                        <span
                             className="font-bold text-slate-800 dark:text-slate-100"
                         >
                             Sign in
-                        </Typography>
+                        </span>
                     </Link>
                 </div>
             )}
-        </AppBar>
+        </div>
     );
 };
 
