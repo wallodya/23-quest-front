4e46e48bef1a6de1d324b16fcb3b846ab603c155
wallodya <78960605+wallodya@users.noreply.github.com>
2023-03-21 15:21:06 +0300

    upd:navBarDesktop refactor

--- src/components/nav/etc/NavBarDesktop.tsx
+++ src/components/nav/etc/NavBarDesktop.tsx
@@ -9,44 +9,13 @@ import MenuIcon from "components/icons/MenuIcon";
 import { useAppSelector } from "store/hooks";
 import PersonIcon from "components/icons/PersonIcon";
 import SettingsIcon from "components/icons/SettingsIcon";
+import { UserMenuDropdownContent, UserMenuDropdownTrigger } from "./NavBarDesktopDropDown";
 
 const PAGE_LINKS: { name: string; link: string }[] = [
     { name: "Home", link: "/home" },
     { name: "Quests", link: "/quests" },
     { name: "New task", link: "/new" },
 ];
-
-const DROPDOWN_LINKS: {
-    name: string;
-    link: string;
-    Icon: () => JSX.Element;
-}[] = [
-    { name: "Profile", link: "/profile", Icon: () => <PersonIcon size="xs" /> },
-    {
-        name: "Settings",
-        link: "/settings",
-        Icon: () => <SettingsIcon size="xs" />,
-    },
-];
-
-const UserMenuDropdownTrigger = () => {
-    const { login } = useAppSelector((state) => state.user);
-
-    return (
-        <DropdownMenu.Trigger asChild>
-            <button
-                className={
-                    "group flex items-center gap-5 rounded-full border border-sky-700 py-1 pl-3 pr-[5px] text-sm font-medium text-sky-700 transition hover:border-sky-800 hover:bg-sky-800 hover:text-slate-100 focus:outline-none focus:ring-4 focus:ring-sky-300 dark:border-sky-600 dark:text-sky-500 dark:hover:border-sky-600 dark:hover:bg-sky-600 dark:hover:text-slate-100 dark:focus:ring-sky-700"
-                }
-            >
-                <span className="">{login}</span>
-                <span className="rounded-full border-sky-700  p-1 group-hover:border-sky-800 dark:border-sky-600 dark:group-hover:border-slate-100">
-                    <PersonIcon size={"xs"} />
-                </span>
-            </button>
-        </DropdownMenu.Trigger>
-    );
-};
 
 const NavLinks = () => (
     <>
@@ -57,22 +26,6 @@ const NavLinks = () => (
             >
                 <Link href={link}>{name}</Link>
             </li>
-        ))}
-    </>
-);
-
-const UserMenuDropdownContent = () => (
-    <>
-        {DROPDOWN_LINKS.map(({ name, link, Icon }, index) => (
-            <DropdownMenu.Item
-                key={index}
-                className="flex items-center gap-2 py-1 focus:outline-none focus:ring-sky-300 dark:focus:ring-sky-700  hover:text-sky-500 dark:hover:text-sky-500"
-            >
-                <Icon />
-                <Link href={link} className="">
-                    {name}
-                </Link>
-            </DropdownMenu.Item>
         ))}
     </>
 );
@@ -95,8 +48,8 @@ const NavBarDesktop = () => {
                     <DropdownMenu.Portal>
                         <DropdownMenu.Content className="mt-4 w-48 rounded-lg bg-slate-200 py-3 px-4 shadow-sm shadow-slate-300 dark:bg-slate-700 dark:shadow-slate-900">
                             <UserMenuDropdownContent />
-                            <DropdownMenu.Separator className="mt-3 border-t border-slate-600" />
-                            <DropdownMenu.Item>
+                            <DropdownMenu.Separator className="mt-3 mb-2 border-t border-slate-600" />
+                            <DropdownMenu.Item className="">
                                 <Button
                                     type="text"
                                     buttonProps={{ onClick: handleSignOut }}
