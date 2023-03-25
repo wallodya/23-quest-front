0694940dd6a28da8b1d51bf848ed7689a5b935a3
wallodya <78960605+wallodya@users.noreply.github.com>
2023-03-25 14:23:43 +0300

    feat(task):foundation

--- src/app/(pages)/tasks/Tasks.tsx
+++ src/app/(pages)/tasks/Tasks.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import TaskCard from './components/TaskCard'
+import { Task } from './task.types'
+
+const TEST_TASKS: Task[] = []
 
 const Tasks = () => {
   return (
-    <div>Tasks</div>
+    <div>
+        <div>{TEST_TASKS.map((task, index) => <TaskCard {...task} key={index}/>)}</div>
+    </div>
   )
 }
 
---
+++ src/app/(pages)/tasks/task.slice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {}
+
+const taskSlice = createSlice({
+    name: "tasks",
+    initialState,
+    reducers: {}
+})
+
+export default taskSlice.reducer
