2b991f2cbc89f7a2b400853b00b669f94a200908
wallodya <78960605+wallodya@users.noreply.github.com>
2023-04-17 15:40:35 +0300

    fix(task): form conditional rendering

--- src/app/(pages)/tasks/components/Form/NewTask.tsx
+++ src/app/(pages)/tasks/components/Form/NewTask.tsx
@@ -7,7 +7,7 @@ import { useAppDispatch, useAppSelector } from "store";
 import { closeTaskForm } from "@task/features";
 import * as Dialog from "@radix-ui/react-dialog";
 import FileIcon from "components/icons/FileIcon";
-import { useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState, useSyncExternalStore } from "react";
 
 const NewTask = ({
     submitTaskFn,
@@ -23,68 +23,80 @@ const NewTask = ({
     isInQuest?: boolean;
 }) => {
     const modalPortal = useRef(null);
-    return (
-        <>
-            <div className="hidden md:block">
-                <Dialog.Root open={isShown} onOpenChange={toggleModal}>
-                    {!isInQuest && (
-                        <Dialog.Trigger asChild>
-                            <button
-                                role="button"
-                                onClick={() => toggleModal(true)}
-                                className="hidden h-full w-full flex-col items-center justify-center gap-2 rounded-xl border-4 border-dashed border-gray-400/40 text-gray-400/40 md:flex "
-                            >
-                                <FileIcon size="sm" />
-                                <span className="font-bold">New Task</span>
-                                <div
-                                    className="fixed top-0 left-0 z-50 hidden md:block"
-                                    ref={modalPortal}
-                                />
-                            </button>
-                        </Dialog.Trigger>
-                    )}
-                    {isInQuest && (
-                        <div
-                            className="fixed top-0 left-0 z-50 hidden md:block"
-                            ref={modalPortal}
-                        />
-                    )}
-                    <Dialog.Portal container={modalPortal.current}>
-                        <Dialog.Overlay className="absolute top-0 left-0 h-screen w-screen bg-gray-900/70" />
-                        <div className="absolute top-0 left-0 flex h-screen w-screen items-center justify-center">
-                            <Dialog.Content
-                                className="
-                         rounded-xl bg-slate-700 px-2"
-                            >
-                                {/* <Dialog.Title>New task</Dialog.Title>
-                            <Dialog.Description>
-                                Create new task form
-                            </Dialog.Description> */}
-                                <NewTaskForm submitTaskFn={submitTaskFn} />
-                                {/* <Dialog.Close /> */}
-                            </Dialog.Content>
-                        </div>
-                    </Dialog.Portal>
-                </Dialog.Root>
-            </div>
+    const TW_MD_WIDTH = 768
 
+    if (typeof window === undefined) {
+        return (
             <div className="md:hidden">
-                <Drawer.Root isOpen={isShown} drawerPosition="bottom">
-                    <Drawer.Content>
-                        <AnimatePresence
-                            onExitComplete={() =>
-                                console.log("step exit complete")
-                            }
+            <Drawer.Root isOpen={isShown} drawerPosition="bottom">
+                <Drawer.Content>
+                    <AnimatePresence
+                        onExitComplete={() =>
+                            console.log("step exit complete")
+                        }
+                    >
+                        <NewTaskForm submitTaskFn={submitTaskFn} />
+                        {/* <Drawer.InnerControls toggleFn={closeForm} /> */}
+                    </AnimatePresence>
+                </Drawer.Content>
+                <Drawer.Background toggleFn={closeForm} />
+            </Drawer.Root>
+        </div>
+        )
+    } else if (window.innerWidth >= TW_MD_WIDTH) {
+        return (
+            <Dialog.Root open={isShown} onOpenChange={toggleModal}>
+                {!isInQuest && (
+                    <Dialog.Trigger asChild>
+                        <button
+                            role="button"
+                            onClick={() => toggleModal(true)}
+                            className="hidden h-full w-full flex-col items-center justify-center gap-2 rounded-xl border-4 border-dashed border-gray-400/40 text-gray-400/40 md:flex "
+                        >
+                            <FileIcon size="sm" />
+                            <span className="font-bold">New Task</span>
+                            <div
+                                className="fixed top-0 left-0 z-50 hidden md:block"
+                                ref={modalPortal}
+                            />
+                        </button>
+                    </Dialog.Trigger>
+                )}
+                {isInQuest && (
+                    <div
+                        className="fixed top-0 left-0 z-50 hidden md:block"
+                        ref={modalPortal}
+                    />
+                )}
+                <Dialog.Portal container={modalPortal.current}>
+                    <Dialog.Overlay className="absolute top-0 left-0 h-screen w-screen bg-gray-900/70" />
+                    <div className="absolute top-0 left-0 flex h-screen w-screen items-center justify-center">
+                        <Dialog.Content
+                            onKeyDown={(event) => (event.key === " " ? {} : {})}
+                            className="
+                 rounded-xl bg-slate-700 px-2"
                         >
                             <NewTaskForm submitTaskFn={submitTaskFn} />
-                            {/* <Drawer.InnerControls toggleFn={closeForm} /> */}
-                        </AnimatePresence>
-                    </Drawer.Content>
-                    <Drawer.Background toggleFn={closeForm} />
-                </Drawer.Root>
-            </div>
-        </>
-    );
+                        </Dialog.Content>
+                    </div>
+                </Dialog.Portal>
+            </Dialog.Root>
+        );
+    } else {
+        return (
+            <Drawer.Root isOpen={isShown} drawerPosition="bottom">
+                <Drawer.Content>
+                    <AnimatePresence
+                        onExitComplete={() => console.log("step exit complete")}
+                    >
+                        <NewTaskForm submitTaskFn={submitTaskFn} />
+                        {/* <Drawer.InnerControls toggleFn={closeForm} /> */}
+                    </AnimatePresence>
+                </Drawer.Content>
+                <Drawer.Background toggleFn={closeForm} />
+            </Drawer.Root>
+        );
+    }
 };
 
 export default NewTask;
