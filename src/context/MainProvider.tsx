cbac4b16cf3af62af3e7a7fa64b0aab325571352
wallodya <78960605+wallodya@users.noreply.github.com>
2023-04-24 13:15:59 +0300

    feat:vercel analytics

--- src/context/MainProvider.tsx
+++ src/context/MainProvider.tsx
@@ -5,15 +5,15 @@ import { Provider } from "react-redux";
 import { store } from "../store/store";
 import { ThemeProvider } from "next-themes";
 import InitUser from "../components/misc/InitUser";
-
+import { Analytics } from "@vercel/analytics/react";
 
 const MainProvider = ({ children }: { children: ReactNode }) => {
     return (
-            <Provider store={store}>
-                {children}
-                <InitUser />
-
-            </Provider>
+        <Provider store={store}>
+            {children}
+            <InitUser />
+            <Analytics />
+        </Provider>
     );
 };
 
