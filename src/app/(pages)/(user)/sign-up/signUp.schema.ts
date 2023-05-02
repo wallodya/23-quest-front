0e63abed462092ebec48fcf50d4852fd481d2ab7
wallodya <78960605+wallodya@users.noreply.github.com>
2023-05-02 23:41:02 +0300

    upd(sign-up):removed password regexes

--- src/app/(pages)/(user)/sign-up/signUp.schema.ts
+++ src/app/(pages)/(user)/sign-up/signUp.schema.ts
@@ -17,10 +17,10 @@ export const SignUpSchema = z
         password: z
             .string()
             .min(4, { message: "Password is too short" })
-            .max(20, { message: "Password can't be longer than 20 letters" })
-            .regex(hasLowerCaseRegex, { message: "Password must include at least 1 lower case letter (a-z)" })
-            .regex(hasUpperCaseRegex, { message: "Password must include at least 1 upper case letter (A-Z)'" })
-            .regex(hasNumberRegex, { message: "Password must include at least 1 number (0-9)" }),
+            .max(20, { message: "Password can't be longer than 20 letters" }),
+            // .regex(hasLowerCaseRegex, { message: "Password must include at least 1 lower case letter (a-z)" })
+            // .regex(hasUpperCaseRegex, { message: "Password must include at least 1 upper case letter (A-Z)'" })
+            // .regex(hasNumberRegex, { message: "Password must include at least 1 number (0-9)" }),
             // .regex(hasSpecialSymbols, { message: "Password must include at least 1 special symbols like '$', '%', etc." })
         confirmPassword: z.string().min(4).max(20),
         terms: z.literal(true, {
